767efaca9afdba3b6a4a4d91ca9d4747a558e577
EC2 Default User <ec2-user@ip-172-31-43-85.ap-northeast-1.compute.internal>
2020-05-19 01:10:16 +0000

    トップページへの遷移するようにした。トップページで質問テーブルのタイトルをすべて表示するようにした

--- app/Http/Controllers/QuestionsController.php
+++ app/Http/Controllers/QuestionsController.php
@@ -2,9 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Question;
+use Auth;
+use Validator;
+
 use Illuminate\Http\Request;
 
 class QuestionsController extends Controller
 {
-    //
+    public function index()
+    {
+        // 質問をすべて取得
+        $questions = Question::get();
+        
+        return view('questions/index',['questions' => $questions]);
+    }
 }
