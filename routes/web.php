460cc72c644500c459e5bc413a4bcd0a84022c03
EC2 Default User <ec2-user@ip-172-31-44-97.ap-northeast-1.compute.internal>
2020-05-20 05:56:45 +0000

    style

--- routes/web.php
+++ routes/web.php
@@ -10,9 +10,11 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+//ログインしている場合のみ
 Route::group(['middleware'=>['auth']],function(){
-    Route::get('newQuestion/','QuestionsController@new');
-    Route::post('storeQuestion/','QuestionsController@store');
+    //質問投稿画面を表示
+    Route::get('/question/new','QuestionsController@new');
+    Route::post('/question/store','QuestionsController@store');
 });
 
 
@@ -30,8 +32,6 @@ Route::get('/login','QuestionsController@login');
 //ユーザー登録画面を表示
 Route::get('/register','QuestionsController@register');
 
-//質問投稿画面を表示
-Route::get('/question/new','QuestionsController@question_new');
 
 //ユーザー詳細画面を表示
 Route::get('/user/show','QuestionsController@user_show');
